acd334c1d0f2b3057e5a9d7d011c2788a5ccd53d
larissa <130381560+larissa2006kskskskskkssks@users.noreply.github.com>
2023-12-10 15:58:31 -0300

    app.js

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("o Paulo fez essa alteraçao enquanto voce nao via a aula"
+console.log("Enviando para o GitHub")
